b885e0fbfb24639eca69b81241d6dc4966510098
AndyChouCoding <andychouforwk@gmail.com>
2024-04-10 13:16:55 +0800

    update traffic light

--- js/traffic_light.js
+++ js/traffic_light.js
@@ -1,33 +1,54 @@
-function switchLight () {
-    const redLight = document.getElementById('red_light');
-    const yellowLight = document.getElementById('yellow_light');
-    const greenLight = document.getElementById('green_light');
+function switchLight() {
+  const redLight = document.getElementById("red_light");
+  const yellowLight = document.getElementById("yellow_light");
+  const greenLight = document.getElementById("green_light");
+  const timerDisplay = document.getElementById("timer");
 
-    function resetLights(){
-        redLight.style.backgroundColor = "grey";
-        yellowLight.style.backgroundColor = "grey";
-        greenLight.style.backgroundColor = "grey";
-    };
+  function resetLights() {
+    redLight.style.backgroundColor = "grey";
+    yellowLight.style.backgroundColor = "grey";
+    greenLight.style.backgroundColor = "grey";
+  }
 
-    function turnOnRed () {
-        resetLights();
-        redLight.style.backgroundColor = "red";
-        setTimeout(turnOnGreen,5000);
-    };
+  function updateTimer(seconds,color) {
+    let count = seconds;
+    timerDisplay.textContent = count;
+    count--;
+    timerDisplay.style.color = color;
+    const interval = setInterval(() => {
+      if (count < 1) {
+        clearInterval(interval);
+        timerDisplay.textContent = "";
+      } else {
+        timerDisplay.textContent = count;
+        timerDisplay.style.color = color;
+      }
+      count--;
+    }, 1000);
+  }
 
-    function turnOnGreen () {
-        resetLights();
-        greenLight.style.backgroundColor = "green";
-        setTimeout(turnOnYellow,5000);
-    };
+  function turnOnRed() {
+    resetLights();
+    redLight.style.backgroundColor = "red";
+    updateTimer(5 , "red");
+    setTimeout(turnOnGreen, 5000);
+  }
 
-    function turnOnYellow () {
-        resetLights();
-        yellowLight.style.backgroundColor = "yellow";
-        setTimeout(turnOnRed,3000);
-    };
-    turnOnRed();
-};
+  function turnOnGreen() {
+    resetLights();
+    greenLight.style.backgroundColor = "green";
+    updateTimer(5 , "green");
+    setTimeout(turnOnYellow, 5000);
+  }
+
+  function turnOnYellow() {
+    resetLights();
+    yellowLight.style.backgroundColor = "yellow";
+    updateTimer(3 , "yellow");
+    setTimeout(turnOnRed, 3000);
+  }
+
+  turnOnRed();
+}
 
 switchLight();
-
