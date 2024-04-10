2c2d8e3efc34d38a089394c9ea4128a136b55061
AndyChouCoding <andychouforwk@gmail.com>
2024-04-10 15:03:26 +0800

    weather api get

---
+++ js/weather.js
@@ -0,0 +1,48 @@
+
+// 替换成你的OpenWeatherMap API密钥
+const apiKey = "2b66b34236af8923813dfbfcdd885a7d";
+const city = "Taipei"; // 可以更换为你想要显示天气的城市
+
+// 获取天气信息
+async function fetchWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('data = ' ,data);
+    // location
+    const location = data.name
+    console.log('location = ' ,location)
+    // weather
+    const weather = data.weather[0].main;
+    console.log('weather = ',weather);
+    // temp
+    const temp = data.main.temp;
+    console.log('temp = ',temp);
+    // time
+    const timeZone = data.timezone;
+    const localTime = calculateLocalTime (timeZone);
+
+    document.getElementById('location').innerText = `${location}`;
+    document.getElementById('time').innerText = `${localTime}`;
+    document.getElementById('weather').innerText =  `${weather}`;
+    document.getElementById('temp').innerText = 'Temp ' + `${temp}`;
+
+}
+
+function calculateLocalTime(timeZone) {
+    const now = new Date();
+    const utcNow = now.getTime() + now.getTimezoneOffset() * 60000;
+    const localTimestamp = utcNow + timeZone * 1000;
+    const localTime = new Date(localTimestamp);
+    const options = {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false 
+    };
+
+    return localTime.toLocaleTimeString('en-US', options);
+}
+
+
+
+fetchWeather();
